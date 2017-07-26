10b651122a884b14b12bbdd9d877459364bb2d7e
Victor Lambret <victor.lambret@sogilis.com>
2017-07-26 19:54:36 +0200

    fix colors

--- tree2ppm.sh
+++ tree2ppm.sh
@@ -6,20 +6,24 @@ OUT=$2
 WIDTH=$(wc -L $IN | cut -d' ' -f 1)
 HEIGHT=$(wc -l $IN | cut -d' ' -f 1)
 
-GREEN="0 255 0 "
-DARK_GREEN="0 128 0 "
-BROWN="192 192 128 "
-DARK_BROWN="128 128 64 "
-BLUE="128 128 255 "
-BLACK="0 0 0 "
+GREEN="0 179 0 "
+DARK_GREEN="0 77 0 "
+BROWN="153 77 0 "
+DARK_BROWN="77 38 0 "
+BLUE="204 255 204 "
+GREY="51 26 0 "
+DARK_GREY="61 61 42 "
 
 echo P3 > $OUT
 echo $WIDTH $HEIGHT >> $OUT
 echo 255 >> $OUT
 
 cat $IN |
-sed -e "s/[^-orTtfT]/${BLUE}/g" |
-sed -e "s/[fF]/${GREEN}/g" |
-sed -e "s/[rtT]/${BROWN}/g" |
-sed -e "s/[-orTtfT]/255 0 255 /g" >> $OUT
+sed -e "s/[^-orTtfFT]/${BLUE}/g" |
+sed -e "s/f/${GREEN}/g" |
+sed -e "s/F/${DARK_GREEN}/g" |
+sed -e "s/t/${BROWN}/g" |
+sed -e "s/[rT]/${DARK_BROWN}/g" |
+sed -e "s/-/${GREY}/g" |
+sed -e "s/o/${DARK_GREY}/g" >> $OUT
 echo >> $OUT
