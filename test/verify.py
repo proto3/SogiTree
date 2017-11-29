84cb1d9158ae8b2ba7a1dd88423bee53f5b73f38
Victor Lambret <victor.lambret@sogilis.com>
2017-11-29 11:27:10 +0100

    Just use test numbers

--- test/verify.py
+++ test/verify.py
@@ -34,37 +34,37 @@ def count_elements(tree_file, elements):
 # TESTS
 ###############################################################################
 
-def test_size(tree_in, tree_out):
+def test_size(test_name, tree_in, tree_out):
     in_height = len(tree_in)
     in_width = len(tree_in[0])
 
     if (in_height != len(tree_out)):
-        return FAILED("Height changed")
+        return FAILED(test_name)
 
     for line in tree_out:
         if (in_width != len(line)):
-            return FAILED("line width modified : \"" + line + "\"")
+            return FAILED(test_name)
 
-    return SUCCESS("size")
+    return SUCCESS(test_name)
 
-def test_aliens(tree_in, tree_out):
+def test_aliens(test_name, tree_in, tree_out):
     for line in tree_out:
         for c in line:
             if c not in ELEMENTS:
-                return FAILED("Invalid element : \"" + c + "\"")
+                return FAILED(test_name)
 
-    return SUCCESS("Use only valid elements")
+    return SUCCESS(test_name)
 
-def test_desert(tree_in, tree_out):
+def test_desert(test_name, tree_in, tree_out):
     green_count_in = count_elements(tree_in, TREE_ELEMENTS)
     green_count_out = count_elements(tree_out, TREE_ELEMENTS)
 
     if (green_count_in == 0 and green_count_out != 0):
-        return FAILED("No tree can grow from the desert")
+        return FAILED(test_name)
 
-    return SUCCESS("A desert can't create new trees")
+    return SUCCESS(test_name)
 
-def test_light(tree_in, tree_out):
+def test_light(test_name, tree_in, tree_out):
     above = [True] * len(tree_in[0])
     light = []
     for line in tree_in:
@@ -78,51 +78,51 @@ def test_light(tree_in, tree_out):
     for y in range(len(tree_out)):
         for x in range(len(tree_out[0])):
             if (tree_out[y][x] in "fFtT" and not light[y][x]):
-                return FAILED("Sun light is useful for trees you known pos="+str((y,x)) + "e=" +tree_out[y][x])
+                return FAILED(test_name)
 
-    return SUCCESS("Trees grow only when sun")
+    return SUCCESS(test_name)
 
-def test_root(tree_in, tree_out):
+def test_root(test_name, tree_in, tree_out):
     for y in range(len(tree_out)):
         for x in range(len(tree_out[0])):
             if (tree_out[y][x] is "r" and tree_in[y][x] not in "r-"):
-                return FAILED("Root can't developp without earth pos="+str((y,x)) + "e=" +tree_in[y][x])
+                return FAILED(test_name)
 
-    return SUCCESS("Roots grows in ground")
+    return SUCCESS(test_name)
 
-def test_old_trunk(tree_in, tree_out):
+def test_old_trunk(test_name, tree_in, tree_out):
     for y in range(len(tree_in)):
         for x in range(len(tree_in[0])):
             if (tree_in[y][x] is "T" and tree_out[y][x] is not "T" ):
-                return FAILED("Old trunk can't disapear !")
+                return FAILED(test_name)
 
-    return SUCCESS("Nothing like an old tree...")
+    return SUCCESS(test_name)
 
-def test_young_trunk(tree_in, tree_out):
+def test_young_trunk(test_name, tree_in, tree_out):
     for y in range(len(tree_in)):
         for x in range(len(tree_in[0])):
             if (tree_in[y][x] is "t" and tree_out[y][x] not in "tT" ):
-                return FAILED("young trunk can only become old")
+                return FAILED(test_name)
 
-    return SUCCESS("You have to care about ")
+    return SUCCESS(test_name)
 
-def test_ground_solidity(tree_in, tree_out):
+def test_ground_solidity(test_name, tree_in, tree_out):
     for y in range(len(tree_in)):
         for x in range(len(tree_in[0])):
             if (tree_in[y][x] is "-" and tree_out[y][x] not in "-r" ):
-                return FAILED("Missing earth")
+                return FAILED(test_name)
 
     for y in range(len(tree_in)):
         for x in range(len(tree_in[0])):
             if (tree_in[y][x] is "o" and tree_out[y][x] is not "o" ):
-                return FAILED("Stop stealing my stones !")
+                return FAILED(test_name)
 
     for y in range(len(tree_out)):
         for x in range(len(tree_out[0])):
             if (tree_out[y][x] in "o-" and tree_out[y][x] is not tree_in[y][x] ):
-                return FAILED("You cant create ground from thin air !")
+                return FAILED(test_name)
 
-    return SUCCESS("Earth looks normal, boring like dirt")
+    return SUCCESS(test_name)
 
 ###############################################################################
 # MAIN
@@ -132,13 +132,13 @@ def main():
     tree_in = create_string_list_from_file(sys.argv[1])
     tree_out = create_string_list_from_file(sys.argv[2])
 
-    test_size(tree_in, tree_out)
-    test_aliens(tree_in, tree_out)
-    test_desert(tree_in, tree_out)
-    test_light(tree_in, tree_out)
-    test_root(tree_in, tree_out)
-    test_old_trunk(tree_in, tree_out)
-    test_young_trunk(tree_in, tree_out)
-    test_ground_solidity(tree_in, tree_out)
+    test_size("TEST1", tree_in, tree_out)
+    test_aliens("TEST2", tree_in, tree_out)
+    test_desert("TEST3", tree_in, tree_out)
+    test_light("TEST4", tree_in, tree_out)
+    test_root("TEST5", tree_in, tree_out)
+    test_old_trunk("TEST6", tree_in, tree_out)
+    test_young_trunk("TEST7", tree_in, tree_out)
+    test_ground_solidity("TEST8", tree_in, tree_out)
 
 main()
